2a7ff56a78f2f1e047551bb40b98922b3ca963ab
Guilherme Vieira <super.driver.512@gmail.com>
2015-07-12 23:28:52 -0300

    Add basic authentication.

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 'use strict';
+let fs = require('fs');
 let express = require('express');
+let basicAuth = require('basic-auth');
 let app = express();
+let passwords = JSON.parse (
+	fs.readFileSync (
+		__dirname + '/passwords.json', {
+			encoding: 'utf8',
+		}
+	)
+);
+app.use(function(req, res, next) {
+	let user = basicAuth(req);
+	function unauthorized() {
+		if(user) {
+			console.log("Authentication failure for user \"" + user.name + "\".");
+		}
+		res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+		res.sendStatus(401);
+	}
+	if(!user) {
+		unauthorized();
+		return;
+	}
+	let password = passwords[user.name];
+	if(!password || user.pass !== password) {
+		unauthorized();
+		return;
+	}
+	req.userName = user.name;
+	next();
+});
 {
 	let port = process.env.PORT || 3000;
 	app.listen(port);
