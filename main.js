a8d7391a480e6b30a25322e245663b515853c066
Guilherme Vieira <super.driver.512@gmail.com>
2015-07-13 00:16:50 -0300

    Implement clipboard functionality.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 let fs = require('fs');
+let hbs = require('handlebars');
 let express = require('express');
 let basicAuth = require('basic-auth');
 let bodyParser = (
@@ -11,6 +12,13 @@ let app = express();
 let passwords = JSON.parse (
 	fs.readFileSync (
 		__dirname + '/passwords.json', {
+			encoding: 'utf8',
+		}
+	)
+);
+let pageTemplate = hbs.compile (
+	fs.readFileSync (
+		__dirname + '/page-template.hbs', {
 			encoding: 'utf8',
 		}
 	)
@@ -37,6 +45,30 @@ app.use(function(req, res, next) {
 	next();
 });
 app.use(bodyParser);
+app.get('/', function(req, res) {
+	res.redirect('/main');
+});
+app.use('/:name', function(req, res, next) {
+	req.clipboardFilePath = (
+		__dirname + '/' + req.userName + '-' + req.params.name +  '.clipboard'
+	);
+	next();
+});
+app.get('/:name', function(req, res) {
+	res.send (
+		pageTemplate ({
+			clipboardData: fs.readFileSync (
+				req.clipboardFilePath, {
+					encoding: 'utf8',
+				}
+			)
+		})
+	);
+});
+app.post('/:name', function(req, res) {
+	fs.writeFileSync(req.clipboardFilePath, req.body.clipboardData);
+	res.redirect('/' + req.params.name);
+});
 {
 	let port = process.env.PORT || 3000;
 	app.listen(port);
